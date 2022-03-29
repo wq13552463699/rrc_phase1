32ed30a0d2a76aad4e7f542af1c0be9a8051b43a
Qiang Wang <73186518+wq13552463699@users.noreply.github.com>
2022-03-29 21:14:35 +0100

    Update replay_buffer_experience.py

--- rrc_example_package/her/rl_modules/replay_buffer_experience.py
+++ rrc_example_package/her/rl_modules/replay_buffer_experience.py
@@ -19,21 +19,18 @@ class replay_buffer:
                         'g': np.empty([self.size, self.T + 1, self.env_params['goal']]),
                         'actions': np.empty([self.size, self.T, self.env_params['action']]),
                         }
-        self.idxs = 0
     
     # store the episode
     def store_episode(self, episode_batch):
         mb_obs, mb_ag, mb_g, mb_actions = episode_batch
         batch_size = mb_obs.shape[0]
-        #idxs = self._get_storage_idx(inc=batch_size)
-        idxs = self.idxs
+        idxs = self._get_storage_idx(inc=batch_size)
         # store the informations
         self.buffers['obs'][idxs] = mb_obs
         self.buffers['ag'][idxs] = mb_ag
         self.buffers['g'][idxs] = mb_g
         self.buffers['actions'][idxs] = mb_actions
         self.n_transitions_stored += self.T * batch_size
-        self.idxs += 1
     
     # sample the data from the replay buffer
     def sample(self, batch_size):
