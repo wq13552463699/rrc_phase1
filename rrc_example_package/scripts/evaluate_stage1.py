67bbd663f4e06c9da7e92fc0a611fb69be7032a5
Qiang Wang <73186518+wq13552463699@users.noreply.github.com>
2022-04-06 19:24:46 +0100

    Update evaluate_stage1.py

--- rrc_example_package/scripts/evaluate_stage1.py
+++ rrc_example_package/scripts/evaluate_stage1.py
@@ -46,7 +46,7 @@ def main():
     step_size=50
     difficulty=3
     obs_type='default'
-    model_path = '/userhome/model_experience_26.pt'
+    model_path = '/userhome/model_experience_27.pt'
     print("The model file I am using is:",model_path)
     #############
     
