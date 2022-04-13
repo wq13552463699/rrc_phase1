985679d08df46c457ab93aeb9cf5e91d6c2f7e4b
Qiang Wang <73186518+wq13552463699@users.noreply.github.com>
2022-04-13 12:37:37 +0100

    Update collect_experience4.py

--- rrc_example_package/scripts/collect_experience4.py
+++ rrc_example_package/scripts/collect_experience4.py
@@ -56,7 +56,7 @@ def main():
     step_size=50
     difficulty=3
     obs_type='default'
-    model_path = '/userhome/model_without_dr_0_1.pt'
+    model_path = '/userhome/model_bext.pt'
     #############
     steps_per_goal=100
     rollouts_length = 90
